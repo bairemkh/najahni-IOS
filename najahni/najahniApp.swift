577781d551589e7a472949e01e39ad6d9fd75ffc
bairemkh <72412872+bairemkh@users.noreply.github.com>
2022-11-07 11:58:13 +0100

    fixing

---
+++ najahni/najahniApp.swift
@@ -0,0 +1,20 @@
+//
+//  najahniApp.swift
+//  najahni
+//
+//  Created by hamabairem on 7/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct najahniApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
