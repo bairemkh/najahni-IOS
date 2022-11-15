28671c6c56e87b7842f65b19646b01185a259dd0
bairemkh <72412872+bairemkh@users.noreply.github.com>
2022-11-15 14:47:24 +0100

    signup fixed

--- najahni/viewmodels/SignupViewModel.swift
+++ najahni/viewmodels/SignupViewModel.swift
@@ -13,11 +13,18 @@ class SignupViewModel: ObservableObject {
     @Published  var password=""
     @Published  var verifPassword=""
     @Published  var roleList=["Trainer","Student"]
-    @Published  var role=0
+    @Published  var role = 0
     @Published  var fieldsList: [ListData] = Fields.allCases.map { feild in
         return ListData(name: feild.rawValue)
     }
-    @Published var selectedFields : [ListData]=[]
+    @Published var selectedFields : [ListData] = []
+    func getListString() -> String {
+        var output = ""
+        selectedFields.forEach { listData in
+            output =  listData.name + " ,"
+        }
+        return output
+    }
     @Published var ibaction=0
     @Published  var sexeList = ["Female", "Male"]
     @Published  var sexe=0
