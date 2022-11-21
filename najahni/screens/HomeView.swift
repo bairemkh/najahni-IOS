502e1a801624072e14102d3647a51916d16d6938
bairemkh <72412872+bairemkh@users.noreply.github.com>
2022-11-21 18:10:53 +0100

    home screen

--- najahni/screens/HomeView.swift
+++ najahni/screens/HomeView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home")
+        VStack{
+            HStack{
+                VStack{
+                    Text("welcome")
+                    Text("welcome")
+                }
+                Image(systemName: "bell")
+                    .foregroundColor(Color("primaryColor"))
+                    .frame(width: 80.0, height: 80.0)
+                    .aspectRatio(contentMode: .fill)
+                WebImage(url: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbvaBdtJ4GaN7m79jU-Y47NqT3Grvxd7qIZ9VKUZKyU1ynYKxoNdlQCixTRDnliBE62os&usqp=CAU"))
+                    .resizable()
+                    .clipShape(Circle())
+                        .shadow(radius: 10)
+                    .padding()
+                    .frame(width: 80.0, height: 80.0)
+                    .aspectRatio(contentMode: .fill)
+                
+            }
+        }
     }
 }
 
