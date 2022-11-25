6c64a7a6f9a7a5989302224f8cd26de5d9906f0f
bairemkh <72412872+bairemkh@users.noreply.github.com>
2022-11-25 06:54:05 -0800

    password

--- najahni/screens/base/customTextField.swift
+++ najahni/screens/base/customTextField.swift
@@ -7,44 +7,79 @@
 
 import SwiftUI
 
-struct customTextField: View {
+struct CustomTextField: View {
     @Binding var text : String
     @State var isHidden = true
     @State private var icon = "eye.slash.fill"
     var body: some View {
-        HStack {
-            if isHidden
-            {
-                SecureField("password", text: $text)
-                    .padding(.all)
-                    .padding(.leading)
-                    .padding(.trailing)
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 1.0, green: 1.0, blue: 1.0)/*@END_MENU_TOKEN@*/)
-                    .cornerRadius(20)
-            }
-            else{
-                TextField("password", text: $text)
-                    .padding(.all)
-                    .padding(.leading)
-                    .padding(.trailing)
-                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 1.0, green: 1.0, blue: 1.0)/*@END_MENU_TOKEN@*/)
-                    .cornerRadius(20)
-            }
-            Image(systemName:icon).onTapGesture {
-                isHidden = !isHidden
-                if isHidden{
-                    icon = "eye.slash.fill"
-                }else{
-                    icon = "eye.fill"
-                }
-            }
+        GroupBox {
+            HStack {
+                 if isHidden
+                 {
+                     SecureField("password", text: $text)
+                         .padding(.all)
+                 }
+                 else{
+                     TextField("password", text: $text)
+                         .padding(.all)
+                 }
+                 Image(systemName:icon).onTapGesture {
+                     isHidden = !isHidden
+                     if isHidden{
+                         icon = "eye.slash.fill"
+                     }else{
+                         icon = "eye.fill"
+                     }
+                 }
+             }
         }
+        .groupBoxStyle(TransparentGroupBox())
+        .shadow(radius: 3)
+        
+           /* HStack {
+                if isHidden
+                {
+                    SecureField("password", text: $text)
+                        .padding(.all)
+                        .padding(.leading)
+                        .padding(.trailing)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 1.0, green: 1.0, blue: 1.0)/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(20)
+                }
+                else{
+                    TextField("password", text: $text)
+                        .padding(.all)
+                        .padding(.leading)
+                        .padding(.trailing)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(red: 1.0, green: 1.0, blue: 1.0)/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(20)
+                }
+                Image(systemName:icon).onTapGesture {
+                    isHidden = !isHidden
+                    if isHidden{
+                        icon = "eye.slash.fill"
+                    }else{
+                        icon = "eye.fill"
+                    }
+                }
+            }*/
+        
     }
 }
 
 struct customTextField_Previews: PreviewProvider {
      @State static var text="password"
     static var previews: some View {
-        customTextField(text: $text)
+        CustomTextField(text: $text)
+    }
+}
+struct TransparentGroupBox: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.content
+            .frame(maxWidth: .infinity)
+            .padding(.leading)
+            .padding(.trailing)
+            .background(RoundedRectangle(cornerRadius: 20).fill(Color.white))
+            .overlay(configuration.label.padding(.leading, 4), alignment: .topLeading)
     }
 }
