10f66dc5eb347f5889d2ba6c8bc003dbdb1c658a
bairemkh <72412872+bairemkh@users.noreply.github.com>
2022-11-28 12:22:36 +0100

    google authentification fixed

--- najahni/utils/constant.swift
+++ najahni/utils/constant.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-let URL_BASE_APP = "http://172.17.0.210:9090"
+let URL_BASE_APP = "http://172.17.13.158:9090"
 //let URL_BASE_APP = "http://localhost:9090"
 
 let LOGIN_URL = URL_BASE_APP + "/user/signin"
@@ -17,6 +17,7 @@ let EDIT_PROFILE = URL_BASE_APP + "/user/editprofile"
 let DELETE_PROFILE = URL_BASE_APP + "/user/delete-account"
 let EDIT_PASSWORD = URL_BASE_APP + "/user/change-password"
 let UPLOAD_IMAGE = URL_BASE_APP + "/user/change-photo"
+let GOOGLE_SIGNIN = URL_BASE_APP + "/user/google-signin"
 
 let ALL_COURSE = URL_BASE_APP + "/course/getall"
 
